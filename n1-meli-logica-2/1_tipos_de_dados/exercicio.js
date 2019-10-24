24bdcc0b4a91f39cdf6ab6a0bf0a0349d111224e
JuMRRibeiro <julianathmartins@gmail.com>
2019-10-23 22:07:47 -0300

    Adding correct folders

---
+++ n1-meli-logica-2/1_tipos_de_dados/exercicio.js
@@ -0,0 +1,11 @@
+//resposta aqui
+var nome = 'Juliana'
+var idade = 32
+var primeiraVez = true
+var lista = ['água', 'papel', 'suco de uva', 'cacau']
+var Cliente = {
+    nome, 
+    idade, 
+    primeiraVez, 
+    lista}
+console.log(Cliente)
